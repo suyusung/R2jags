e41c3620c9de70d64e8204ecdeac89178dd5096e
Gianluca Baio <g.baio@ucl.ac.uk>
2024-06-23 19:39:56 +0100

    Update 'adapt' in 'jags.R' to force progress.bar to 'none' and quiet to TRUE, irrespective of the values set in the call to the function 'jags'

--- R/jags.R
+++ R/jags.R
@@ -156,8 +156,10 @@ jags <- function( data, inits,
                   n.adapt  = 0,
                   quiet = quiet )
   
-  # Adds adaptation, just in case...
-  adapt(m,n.iter=n.adapt,progress.bar=progress.bar,quiet=quiet,end.adaptation=TRUE)
+  # Adds adaptation. This never needs to be verbose, so force
+  # 'progress.bar' to 'none' and 'quiet' to TRUE, irrespective of what the
+  # global values for these options are in the function
+  adapt(m,n.iter=n.adapt,progress.bar="none",quiet=TRUE,end.adaptation=TRUE)
 
   # Updates the model for the burning phase
   .update.jags(   
