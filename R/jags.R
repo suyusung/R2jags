a2a5749a541a7654911f854c46996275d6fe13d0
Gianluca Baio <g.baio@ucl.ac.uk>
2024-06-23 19:25:08 +0100

    Updated with upstream but keeps local changes to defaults in 'jags.R' and in 'print.R'

--- R/jags.R
+++ R/jags.R
@@ -158,9 +158,6 @@ jags <- function( data, inits,
   # Adds adaptation, just in case...
   adapt(m,n.iter=n.adapt,progress.bar=progress.bar,quiet=quiet,end.adaptation=TRUE)
 
-  # Adds adaptation
-  adapt(m,n.iter=n.adapt,quiet=TRUE, progress.bar=progress.bar, end.adaptation=TRUE)
-  
   # Updates the model for the burning phase
   .update.jags( 
     m,
