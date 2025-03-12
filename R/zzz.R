9965c6ff0afc0d10de6c5edf7063c54d62725f2d
Gianluca Baio <g.baio@ucl.ac.uk>
2025-03-12 11:37:55 +0000

    Add onload message with version of the package

--- R/zzz.R
+++ R/zzz.R
@@ -11,3 +11,12 @@
     options("r2j.print.program"=TRUE)
   }  
 }
+
+#' @title .onAttach
+#' @description prints out a friendly reminder message to the user
+#' @inheritParams base .onAttach
+#' @return NULL
+#' @noRd
+.onAttach <- function(libname, pkgname) {
+  packageStartupMessage("The R2jags version loaded is: ", utils::packageVersion("R2jags"))
+}
